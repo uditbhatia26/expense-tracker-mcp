d9ca552b79e8486c8547fc557828a4a09ef3958d
Udit <130693818+uditbhatia26@users.noreply.github.com>
2026-01-30 00:03:06 +0530

    Uncomment main execution block for remote server

--- main.py
+++ main.py
@@ -82,5 +82,5 @@ def categories():
 
 
 # To conver this into a remote server
-# if __name__ == "__main__":
-#     mcp.run(transport='http', host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    mcp.run(transport='http', host='0.0.0.0', port=8000)
